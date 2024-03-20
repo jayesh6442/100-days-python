69bb29c5c61ccfadb965e6f0b3eb736f346798e2
jayesh sarvaiya <jayeshsarvaiya9913@gmail.com>
2024-03-20 11:50:51 +0530

    add some methods

--- day17/day17_pycharm/main.py
+++ day17/day17_pycharm/main.py
@@ -1,1 +1,12 @@
-print("we are in the day 17")
+# class User:
+#     def __init__(self,name,roll):
+#         self.name = name
+#         self.roll = roll
+#         self.followers = 0
+#
+# u1 = User("jayesh",34)
+# u2 = User("savan",1)
+#
+# print(u1.name)
+# print(u1.roll)
+# print(u1.followers)
